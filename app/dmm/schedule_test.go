32de2a4b631c7ab0fa64c007a79afec794e9ba21
Toshiki Watanabe <toshikwa@gmail.com>
2023-05-03 14:42:26 +0000

    Update tests

--- app/dmm/schedule_test.go
+++ app/dmm/schedule_test.go
@@ -2,34 +2,57 @@ package dmm
 
 import (
 	"reflect"
+	"strconv"
 	"testing"
 )
 
 func TestDiffSlots(t *testing.T) {
 	t.Helper()
-	news := []Slot{
-		{TeacherId: "a", DateTime: "11-14_22-30"},
-		{TeacherId: "a", DateTime: "11-24_11-30"},
-		{TeacherId: "a", DateTime: "03-05_08-30"},
-		{TeacherId: "a", DateTime: "08-30_00-00"},
-		{TeacherId: "a", DateTime: "06-21_11-30"},
+	tests := []map[string][]Slot{
+		{
+			"news": {
+				{TeacherId: "a", DateTime: "11-14_22-30"},
+				{TeacherId: "a", DateTime: "11-24_11-30"},
+				{TeacherId: "a", DateTime: "03-05_08-30"},
+				{TeacherId: "a", DateTime: "08-30_00-00"},
+				{TeacherId: "a", DateTime: "06-21_11-30"},
+			},
+			"exists": {
+				{TeacherId: "a", DateTime: "11-24_11-30"},
+				{TeacherId: "a", DateTime: "03-30_01-00"},
+				{TeacherId: "a", DateTime: "11-14_22-30"},
+				{TeacherId: "a", DateTime: "06-21_11-30"},
+				{TeacherId: "a", DateTime: "03-05_08-30"},
+			},
+			"adds": {{TeacherId: "a", DateTime: "08-30_00-00"}},
+			"dels": {{TeacherId: "a", DateTime: "03-30_01-00"}},
+		},
+		{
+			"news": {
+				{TeacherId: "a", DateTime: "11-14_22-30"},
+			},
+			"exists": {},
+			"adds":   {{TeacherId: "a", DateTime: "11-14_22-30"}},
+			"dels":   {},
+		},
+		{
+			"news":   {},
+			"exists": {{TeacherId: "a", DateTime: "11-14_22-30"}},
+			"adds":   {},
+			"dels":   {{TeacherId: "a", DateTime: "11-14_22-30"}},
+		},
 	}
-	exists := []Slot{
-		{TeacherId: "a", DateTime: "11-24_11-30"},
-		{TeacherId: "a", DateTime: "03-30_01-00"},
-		{TeacherId: "a", DateTime: "11-14_22-30"},
-		{TeacherId: "a", DateTime: "06-21_11-30"},
-		{TeacherId: "a", DateTime: "03-05_08-30"},
-	}
-	want := map[string][]Slot{
-		"adds": {{TeacherId: "a", DateTime: "08-30_00-00"}},
-		"dels": {{TeacherId: "a", DateTime: "03-30_01-00"}},
-	}
-	adds, dels, _ := DiffSlots(news, exists)
-	if !reflect.DeepEqual(adds, want["adds"]) {
-		t.Errorf("got %+v, want %+v", adds, want["adds"])
-	}
-	if !reflect.DeepEqual(dels, want["dels"]) {
-		t.Errorf("got %+v, want %+v", adds, want["dels"])
+	for i, tt := range tests {
+		tt := tt
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel()
+			adds, dels, _ := DiffSlots(tt["news"], tt["exists"])
+			if !reflect.DeepEqual(adds, tt["adds"]) {
+				t.Errorf("got %+v, want %+v", adds, tt["adds"])
+			}
+			if !reflect.DeepEqual(dels, tt["dels"]) {
+				t.Errorf("got %+v, want %+v", dels, tt["dels"])
+			}
+		})
 	}
 }
